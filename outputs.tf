0004ad9e1cac102949502f69c125b95c5a4da642
Jerry Chang <therealjerrycan@gmail.com>
2020-08-08 14:47:03 -0700

    Update queue to output queue url

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,12 @@
-output "aws_iam_user" {
-    value = aws_iam_user.dev_user
-}
-
 output "aws_iam_access_id" {
     value = aws_iam_access_key.dev_key.id
 }
 
 output "aws_iam_access_key" {
     value = aws_iam_access_key.dev_key.secret
+}
+
+output "aws_queue_url" {
+    value = aws_sqs_queue.ingest_queue.id
+
 }

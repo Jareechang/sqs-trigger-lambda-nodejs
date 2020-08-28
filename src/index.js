17445d081ca6f7d72ad77261e07d516180bb7df8
Jerry Chang <therealjerrycan@gmail.com>
2020-08-27 21:47:02 -0700

    Add conditional in lambda to throw if we receive a certain type of message

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+function parseSqsBodyMessage(sqsRecord) {
+    const body = sqsRecord && sqsRecord.body;
+    let result = {};
+    try {
+        result = JSON.parse(body);
+    } catch (ex) {
+        console.error('Error parsing sqs message body: ', ex);
+    }
+
+    return result;
+}
+
 exports.handler = async function (event, context) {
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+    const records = event['Records'] || [];
+
+    const errorRecords = records.filter((record) => {
+        const bodyMessage = parseSqsBodyMessage(record);
+        return bodyMessage.type === 'ERROR';
+    });
+
+    if (errorRecords.length > 0) {
+        throw new Error('Received error from SQS message body');
+    }
+
+    console.log('SQS Records: ', JSON.stringify(records, null, 4));
 }
